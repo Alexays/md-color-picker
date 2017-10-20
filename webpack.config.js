fea0cabafb243dbadb31445378f713a645d9f273
Alexays <alexisr245@gmail.com>
2017-10-20 13:56:48 +0200

    chore() update prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 let path = require('path');
 let BUILD_DIR = path.resolve(__dirname, 'lib');
 let APP_DIR = path.resolve(__dirname, 'src');
+let MODULE_DIR = path.resolve(__dirname, 'node_modules/@material');
 const BabiliPlugin = require("babili-webpack-plugin");
 
 module.exports = {
@@ -11,38 +12,40 @@ module.exports = {
   output: {
     path: BUILD_DIR,
     filename: 'MdColorPicker.js',
-    library:'MdColorPicker',
+    library: 'MdColorPicker',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
   stats: {
-    warning: false,
+    warnings: false,
   },
   module: {
     loaders: [{
-        test: /\.jsx?/,
-        include: APP_DIR,
-        loader: 'babel-loader',
+      test: /\.js$/,
+      include: [MODULE_DIR, APP_DIR],
+      use: [
+        'babel-loader',
+      ],
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader"
       }, {
-        test: /\.scss$/,
-        use: [{
-          loader: "style-loader"
-        }, {
-          loader: "css-loader"
-        }, {
-          loader: "sass-loader",
-          options: {
-            includePaths: ['node_modules', 'src']
-          }
-        }]
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader',
-      }
+        loader: "css-loader"
+      }, {
+        loader: "sass-loader",
+        options: {
+          includePaths: ['node_modules', 'src']
+        }
+      }]
+    },
+    {
+      test: /\.svg$/,
+      loader: 'svg-inline-loader',
+    }
     ],
   },
-    plugins: [
-      new BabiliPlugin()
-    ]
+  plugins: [
+    new BabiliPlugin()
+  ]
 }
